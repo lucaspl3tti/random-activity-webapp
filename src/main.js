993456ad6c1f674945f1c3c27db906d7514f22ac
Jan-Luca Splettstößer <jan-luca@spletti-din.de>
2022-02-27 00:10:34 +0100

    Remove cypress from project

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
 // Define Variables for the game
