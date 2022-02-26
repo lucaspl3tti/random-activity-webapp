33ac9a049f9e0150a0d027dd153900a64d4ffa3f
Jan-Luca Splettstößer <jan-luca@spletti-din.de>
2022-02-27 00:18:41 +0100

    Update vue router and package.json

--- src/router/index.js
+++ src/router/index.js
@@ -6,25 +6,16 @@ const router = createRouter({
     {
       path: '/',
       name: 'start-game',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/StartGame.vue')
     },
     {
       path: '/start',
       name: 'add-players',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AddPlayers.vue')
     },
     {
       path: '/activity',
       name: 'activity',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/Activity.vue')
     },
   ]
